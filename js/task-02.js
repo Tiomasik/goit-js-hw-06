b0f325609e50077b9ade9c7ef5486c383676b9dd
Tiomasik <a.masliy@ukr.net>
2022-10-16 01:56:43 +0300

    make 1, 2 tasks

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector(`#ingredients`);
+
+const makeListItem = items => {
+  return items.map(element => {
+    const itemEl = document.createElement(`li`);
+    itemEl.textContent = element;
+    itemEl.classList.add(`item`);
+    console.log(itemEl);
+    return itemEl
+  })
+}
+
+const elements = makeListItem(ingredients);
+
+listEl.append(...elements);
+
