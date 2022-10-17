9fa651331a73be4816cefbd4693cd07d1d6123fa
Tiomasik <a.masliy@ukr.net>
2022-10-17 12:08:20 +0300

    made 9 task

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,26 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const startsizeBox = 30;
+const numberBoxes = {};
+
+const btnCreateEl = document.querySelector(`button[data-create]`);
+const btnDestroyEl = document.querySelector(`button[data-destroy]`);
+const inputEl = document.querySelector(`input`)
+
+inputEl.addEventListener(`input`, onChangeBoxSize)
+// btnCreateEl.addEventListener(`click`, createBoxes)
+
+function onChangeBoxSize(event) {
+  // console.log(event.currentTarget.valueAsNumber);
+  createBoxes(event.currentTarget.valueAsNumber)
+}
+
+function createBoxes(amount) {
+  // console.log(amount);
+  const sizeBox = `${startsizeBox + (amount - 1) * 10}px`;
+  
+  console.log(sizeBox);
+}
+
